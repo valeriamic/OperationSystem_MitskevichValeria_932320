bdf645f0fdcf5ac4de26fd7ec9ebe86d7d1f00a0
valeriamic <mickeviclera@gmail.com>
2025-12-23 12:19:00 +0700

    Update tsulab.c

--- tsulab.c
+++ tsulab.c
@@ -7,29 +7,28 @@
 #include <linux/version.h>
 
 #define PROCFS_NAME "tsulab"
-
-#define HALE_BOPP_PERIHELION_EPOCH_SEC 859334400 
-
-#define SECONDS_IN_DAY (24 * 60 * 60)
+#define HALE_BOPP_PERIHELION_EPOCH_SEC 859334400LL 
+#define SECONDS_IN_DAY (24 * 60 * 60LL)
 
 static struct proc_dir_entry *our_proc_file;
 
 static ssize_t procfile_read(struct file *file_pointer, char __user *buffer,
                              size_t buffer_length, loff_t *offset)
 {
-    char s[32];
+    char s[64];
     int len = 0;
+    struct timespec64 current_time;
+    long long diff_sec;
+    long long days;
 
     if (*offset > 0) {
         return 0;
     }
 
-    struct timespec64 current_time;
     ktime_get_real_ts64(&current_time);
     
-    long long diff_sec = current_time.tv_sec - HALE_BOPP_PERIHELION_EPOCH_SEC;
-
-    long long days = diff_sec / SECONDS_IN_DAY;
+    diff_sec = current_time.tv_sec - HALE_BOPP_PERIHELION_EPOCH_SEC;
+    days = diff_sec / SECONDS_IN_DAY;
     
     len = snprintf(s, sizeof(s), "%lld\n", days);
 
@@ -42,9 +41,6 @@ static ssize_t procfile_read(struct file *file_pointer, char __user *buffer,
     }
 
     *offset += len;
-    
-    pr_info("tsulab: /proc/%s read. Days calculated: %lld\n", PROCFS_NAME, days);
-    
     return len;
 }
 
@@ -55,21 +51,20 @@ static const struct proc_ops proc_file_fops = {
 #else
 static const struct file_operations proc_file_fops = {
     .read = procfile_read,
+    .owner = THIS_MODULE,
 };
 #endif
 
 static int __init tsulab_init(void)
 {
-    pr_info("Welcome to the Tomsk State University\n");
-
     our_proc_file = proc_create(PROCFS_NAME, 0444, NULL, &proc_file_fops);
 
-    if (our_proc_file == NULL) {
-        remove_proc_entry(PROCFS_NAME, NULL);
-        pr_alert("tsulab: Error: Could not create /proc/%s\n", PROCFS_NAME);
+    if (!our_proc_file) {
+        pr_err("tsulab: Failed to create /proc/%s\n", PROCFS_NAME);
         return -ENOMEM;
     }
     
+    pr_info("Welcome to the Tomsk State University\n");
     pr_info("tsulab: /proc/%s created successfully.\n", PROCFS_NAME);
     
     return 0;
@@ -77,9 +72,11 @@ static int __init tsulab_init(void)
 
 static void __exit tsulab_exit(void)
 {
-    remove_proc_entry(PROCFS_NAME, NULL);
-    pr_info("tsulab: /proc/%s removed.\n", PROCFS_NAME);
+    if (our_proc_file) {
+        remove_proc_entry(PROCFS_NAME, NULL);
+    }
     
+    pr_info("tsulab: /proc/%s removed.\n", PROCFS_NAME);
     pr_info("Tomsk State University forever!\n");
 }
 
